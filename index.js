27edff91f58c0f3c878bf1922ad4edd0ab2aa8be
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-09-22 01:10:02 -0500

    estructura slider funcionando

--- index.js
+++ index.js
@@ -9,15 +9,36 @@ console.log(sliderLast)
 
 left.addEventListener("click", function(){
 
+    let sliderfirst = document.querySelectorAll(".slider")[0]
 
-    content_slider.style.left = '-100%';
+    content_slider.style.marginLeft = "-200%"
+    content_slider.style.transition = "all 0.5s"
+
     content_slider.insertAdjacentElement("beforeend", sliderLast)
+
+    
+    setTimeout(function(){
+        content_slider.style.transition = "none"
+        content_slider.insertAdjacentElement("beforeend", sliderfirst)
+          content_slider.style.marginLeft = "-100%"    },1000)
+   
+
 
 })
 
 
 rigth.addEventListener("click", function(){
-    content_slider.style.left = '0%';
+    content_slider.style.marginLeft = "0%"
+    content_slider.style.transition = "all 0.5s"
     content_slider.insertAdjacentElement("afterbegin", sliderLast)
+
+   setTimeout(function(){
+    content_slider.style.transition = "none"
+    content_slider.style.marginLeft = "-100%" 
+
+
+   },1000
+
+   )
 
 })
